d2d76365db51a00ea4545722f6d7082a9430b00b
amytang0 <atang@plaid.com>
2020-11-16 17:46:10 -0800

    uncomment

--- app/src/main/java/com/plaid/linksample/network/LinkTokenRequester.kt
+++ app/src/main/java/com/plaid/linksample/network/LinkTokenRequester.kt
@@ -33,8 +33,8 @@ object LinkTokenRequester {
       .map { it.link_token }
 
   //Comment out the above and uncomment the below to use a curled Link Token
-  val token
-    get() = Single.just("<LINK TOKEN HERE">)
+  //val token
+  //  get() = Single.just("<GENERATED_LINK_TOKEN>">)
 
 }
 
