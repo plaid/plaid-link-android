9f6e8b4e2d3003ae4d03eb382629b72c545cfd77
TadowNow <74590285+TadowNow@users.noreply.github.com>
2020-11-19 20:26:19 -0600

    TadowNow
    In home services for elderly

--- app/src/main/java/com/plaid/linksample/MainActivityJava.java
+++ app/src/main/java/com/plaid/linksample/MainActivityJava.java
@@ -1,5 +1,25 @@
-/*
- * Copyright (c) 2020 Plaid Technologies, Inc. <support@plaid.com>
+/app/src/main/java/com/plaid/linksample/MainActivityJava.java
+ * Copyrconst response = await client
+  .createLinkToken({
+    user: {
+      client_user_id: '123-test-user-id',
+    },
+    client_name: 'Plaid Test App',
+    products: ['auth', 'transactions'],
+    country_codes: ['GB'],
+    language: 'en',
+    webhook: 'https://sample-web-hook.com',
+    account_filters: {
+      depository: {
+        account_subtypes: ['checking', 'savings'],
+      },
+    },
+  })
+  .catch((err) => {
+    // handle error
+  });
+
+const linkToken = response.link_token;ight (c) 2020 Plaid Technologies, Inc. <support@plaid.com>
  */
 
 package com.plaid.linksample;
