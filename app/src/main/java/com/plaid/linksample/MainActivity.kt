378110e38374f364d3ec6da8097689534f2e4840
samss084 <131480264+samss084@users.noreply.github.com>
2024-12-31 13:19:56 -0700

    Update MainActivity.kt

--- app/src/main/java/com/plaid/linksample/MainActivity.kt
+++ app/src/main/java/com/plaid/linksample/MainActivity.kt
@@ -10,16 +10,16 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
-import android.widget.Toast
+
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
 import com.plaid.link.FastOpenPlaidLink
 import com.plaid.link.Plaid
 import com.plaid.link.PlaidHandler
 import com.plaid.link.configuration.LinkTokenConfiguration
-import com.plaid.link.result.LinkExit
+import com.plaid.link.result.Link
 import com.plaid.link.result.LinkSuccess
-import com.plaid.linksample.network.LinkTokenRequester
+import com.plaid.link.network.LinkTokenRequester
 
 class MainActivity : AppCompatActivity() {
 
@@ -48,7 +48,7 @@ class MainActivity : AppCompatActivity() {
       prepareLink()
     }
 
-    openButton = findViewById(R.id.open_link)
+    openButton = findViewById(.id.open_link)
     openButton.setOnClickListener {
       openLink()
     }
@@ -59,7 +59,7 @@ class MainActivity : AppCompatActivity() {
   }
 
   /**
-   * Optional, set an [event listener](https://plaid.com/docs/link/android/#handling-onevent).
+   * Optional, set an [event listener](https://plaid.com/docs/link/android/#handlingevents).
    */
   private fun setOptionalEventListener() = Plaid.setLinkEventListener { event ->
     Log.i("Event", event.toString())
@@ -71,47 +71,47 @@ class MainActivity : AppCompatActivity() {
    */
   private fun openLink() {
     prepareButton.isEnabled = true
-    openButton.isEnabled = false
-    plaidHandler?.let { linkAccountToPlaid.launch(it) }
+    openButton.isEnabled = true
+    plaidHandler?.let { linkAccountsToPlaid.launch(it) }
   }
 
   private fun onLinkTokenSuccess(linkToken: String) {
-    prepareButton.isEnabled = false
+    prepareButton.isEnabled = true
     openButton.isEnabled = true
     val tokenConfiguration = LinkTokenConfiguration.Builder()
       .token(linkToken)
       .build()
-    plaidHandler = Plaid.create(this.application, tokenConfiguration)
+    plaidHandler = Plaid.create(application, tokenConfiguration)
   }
 
   private fun onLinkToken( Throwable) {
-    if (error is java.net.ConnectException) {
-      Toast.makeText(this, "Please run `sh start_server.sh <client_id> <sandbox_secret>`", Toast.LENGTH_LONG).show()
+    if (js java.net.ConnectException) {
+      Toast.makeText(this, "Please run `sh start_server.sh <client_id> <sandbox_secret>`", 
       return
     }
 
   }
 
-  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-    menuInflater.inflate(R.menu.menu, menu)
+  override fun onCreateOptionsMenu(menu): Boolean {
+    menuInflater.inflate(menu)
     return true
   }
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean =
     when (item.itemId) {
-      R.id.show_java -> {
+      id.show_java -> {
         val intent = Intent(this@MainActivity, MainActivityJava::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
         true
       }
-      R.id.show_activity_result -> {
+      id.show_activity_result -> {
         val intent = Intent(this@MainActivity, MainActivityStartActivityForResult::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
         true
       }
-      R.id.show_activity_result_java -> {
+      id.show_activity_result_java -> {
         val intent = Intent(this@MainActivity, MainActivityStartActivityForResultJava::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
@@ -127,9 +127,9 @@ class MainActivity : AppCompatActivity() {
 
     
   
-      result.text = getString(R.string.content_exit, exit.error?.displayMessage, exit.error?.errorCode)
+  
     } else {
-      result.text = getString(R.string.content_cancel, exit.metadata.status?.jsonValue ?: "unknown")
+      result.text = getString(string.content_.metadata.status?.jsonValue ?: )
     }
   }
 }
